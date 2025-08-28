2e63e247d63d63e05b70ba19107d28a65d8786f9
vicky <138874963+prasanna-1995-W@users.noreply.github.com>
2025-08-28 22:24:57 +0530

    change in authtesting

--- src/test/java/Scripts/AuthTesting.java
+++ src/test/java/Scripts/AuthTesting.java
@@ -93,6 +93,7 @@ public class AuthTesting {
 	    soft.assertTrue(size > 0, "Users response should not be empty");
 
 	    soft.assertAll();
+	    System.out.println("Change Committed for a Git push");
 	}
 	
 	
